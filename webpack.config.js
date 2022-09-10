6f1ba8da23599346743d4182b5a51e119768d124
tomcoso <tomasdessy0@gmail.com>
2022-09-10 17:39:37 +0100

    finish setup (add babel for jest)

--- webpack.config.js
+++ webpack.config.js
@@ -7,28 +7,28 @@ module.exports = {
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Battleship',
-      template: './src/template.html'
-    })
+      template: './src/template.html',
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: ['style-loader', 'css-loader', 'sass-loader']
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource'
+        type: 'asset/resource',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource'
-      }
-    ]
-  }
+        type: 'asset/resource',
+      },
+    ],
+  },
 }
