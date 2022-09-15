d0b4b717128e5bba9bf3427920725f4567acc4f4
tomcoso <tomasdessy0@gmail.com>
2022-09-15 16:24:57 +0100

    made AI smarter (may actually win)

--- src/modules/player.js
+++ src/modules/player.js
@@ -11,15 +11,48 @@ const Player = function (type) {
     return obs.publish('attack', [enemy, index])
   }
 
+  let firstHit
+  let nextIndex = []
+
+  const getAdjacent = function (index) {
+    const array = []
+    array.push([index[0], index[1] - 1])
+    array.push([index[0], index[1] + 1])
+    array.push([index[0] - 1, index[1]])
+    array.push([index[0] + 1, index[1]])
+    return array
+  }
+
   const play = function (enemyBoard, enemy) {
     if (type === 'user') throw new Error('Users cannot use play method!')
 
+    if (firstHit && enemyBoard[firstHit[0]][firstHit[1]].ship.isSunk()) {
+      firstHit = undefined
+      nextIndex = []
+    }
+
     let index
+
     while (true) {
-      index = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)]
+      if (nextIndex.length > 0) {
+        index = nextIndex.pop()
+        if (index[0] > 9 || index[0] < 0 || index[1] > 9 || index[1] < 0)
+          continue
+      } else {
+        index = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)]
+      }
+
       const target = enemyBoard[index[0]][index[1]]
-      if (target === undefined) break
-      if (typeof target === 'object' && target.section.status === 'ok') break
+
+      if (target === undefined) {
+        break
+      }
+      if (typeof target === 'object' && target.section.status === 'ok') {
+        if (!firstHit) firstHit = index
+        const newTargets = getAdjacent(index)
+        newTargets.forEach((x) => nextIndex.push(x))
+        break
+      }
     }
     return attack(index, enemy)
   }
